568861f7bbbacc5edd9eb5816459b19018db027a
Denys Plevako <game.frans@gmail.com>
2025-01-15 16:09:33 +0100

    Update BlackboardHelper.cs

--- Runtime/BehaviourTree/ECS/Blackboard/BlackboardHelper.cs
+++ Runtime/BehaviourTree/ECS/Blackboard/BlackboardHelper.cs
@@ -2,14 +2,17 @@ using SD.ECSBT.BehaviourTree.Data;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace SD.ECSBT.BehaviourTree.ECS.Blackboard
 {
     [BurstCompile]
     public static class BlackboardHelper
     {
+        // Entity
         [BurstCompile]
-        public static void TryUpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
             in FixedString32Bytes blackboardId, in Entity value, in Entity btInstance)
         {
             var shouldNotify = false;
@@ -32,9 +35,62 @@ namespace SD.ECSBT.BehaviourTree.ECS.Blackboard
             if (!shouldNotify) return;
             CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
         }
-
+        
+        // quaternion
         [BurstCompile]
-        public static void TryUpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
+            in FixedString32Bytes blackboardId, in quaternion value, in Entity btInstance)
+        {
+            var shouldNotify = false;
+            var notifyType = NotifyType.OnValueChange;
+            if (blackboardData.QuaternionVars.TryGetValue(blackboardId, out var currentValue))
+            {
+                if (math.any(currentValue.value != value.value))
+                {
+                    blackboardData.QuaternionVars[blackboardId] = value;
+                    shouldNotify = true;
+                }
+            }
+            else
+            {
+                notifyType = NotifyType.OnValueExistStateChange;
+                blackboardData.QuaternionVars.Add(blackboardId, value);
+                shouldNotify = true;
+            }
+            
+            if (!shouldNotify) return;
+            CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
+        }
+        
+        // string
+        [BurstCompile]
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
+            in FixedString32Bytes blackboardId, in FixedString32Bytes value, in Entity btInstance)
+        {
+            var shouldNotify = false;
+            var notifyType = NotifyType.OnValueChange;
+            if (blackboardData.StringVars.TryGetValue(blackboardId, out var currentValue))
+            {
+                if (currentValue != value)
+                {
+                    blackboardData.StringVars[blackboardId] = value;
+                    shouldNotify = true;
+                }
+            }
+            else
+            {
+                notifyType = NotifyType.OnValueExistStateChange;
+                blackboardData.StringVars.Add(blackboardId, value);
+                shouldNotify = true;
+            }
+            
+            if (!shouldNotify) return;
+            CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
+        }
+
+        // bool
+        [BurstCompile]
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
             in FixedString32Bytes blackboardId, in bool value, in Entity btInstance)
         {
             var shouldNotify = false;
@@ -57,7 +113,110 @@ namespace SD.ECSBT.BehaviourTree.ECS.Blackboard
             if (!shouldNotify) return;
             CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
         }
+        
+        // int
+        [BurstCompile]
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
+            in FixedString32Bytes blackboardId, in int value, in Entity btInstance)
+        {
+            var shouldNotify = false;
+            var notifyType = NotifyType.OnValueChange;
+            if (blackboardData.IntVars.TryGetValue(blackboardId, out var currentValue))
+            {
+                if (!Mathf.Approximately(currentValue, value))
+                {
+                    blackboardData.IntVars[blackboardId] = value;
+                    shouldNotify = true;
+                }
+            }
+            else
+            {
+                notifyType = NotifyType.OnValueExistStateChange;
+                blackboardData.IntVars.Add(blackboardId, value);
+                shouldNotify = true;
+            }
+            
+            if (!shouldNotify) return;
+            CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
+        }
+        
+        // float
+        [BurstCompile]
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb,
+            in FixedString32Bytes blackboardId, in float value, in Entity btInstance)
+        {
+            var shouldNotify = false;
+            var notifyType = NotifyType.OnValueChange;
+            if (blackboardData.FloatVars.TryGetValue(blackboardId, out var currentValue))
+            {
+                if (!Mathf.Approximately(currentValue, value))
+                {
+                    blackboardData.FloatVars[blackboardId] = value;
+                    shouldNotify = true;
+                }
+            }
+            else
+            {
+                notifyType = NotifyType.OnValueExistStateChange;
+                blackboardData.FloatVars.Add(blackboardId, value);
+                shouldNotify = true;
+            }
+            
+            if (!shouldNotify) return;
+            CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
+        }
+        
+        // float2
+        [BurstCompile]
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb, FixedString32Bytes blackboardId, float2 value, in Entity btInstance)
+        {
+            var shouldNotify = false;
+            var notifyType = NotifyType.OnValueChange;
+            if (blackboardData.Float2Vars.TryGetValue(blackboardId, out var currentValue))
+            {
+                if (math.any(currentValue != value))
+                {
+                    blackboardData.Float2Vars[blackboardId] = value;
+                    shouldNotify = true;
+                }
+            }
+            else
+            {
+                notifyType = NotifyType.OnValueExistStateChange;
+                blackboardData.Float2Vars.Add(blackboardId, value);
+                shouldNotify = true;
+            }
+            
+            if (!shouldNotify) return;
+            CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
+        }
+        
+        // float3
+        [BurstCompile]
+        public static void UpdateVar(ref BlackboardData blackboardData, ref EntityCommandBuffer ecb, FixedString32Bytes blackboardId, float3 value, in Entity btInstance)
+        {
+            var shouldNotify = false;
+            var notifyType = NotifyType.OnValueChange;
+            if (blackboardData.Float3Vars.TryGetValue(blackboardId, out var currentValue))
+            {
+                if (math.any(currentValue != value))
+                {
+                    blackboardData.Float3Vars[blackboardId] = value;
+                    shouldNotify = true;
+                }
+            }
+            else
+            {
+                notifyType = NotifyType.OnValueExistStateChange;
+                blackboardData.Float3Vars.Add(blackboardId, value);
+                shouldNotify = true;
+            }
+            
+            if (!shouldNotify) return;
+            CreateNotifyCommand(ref ecb, blackboardId, notifyType, btInstance);
+        }
 
+        [BurstCompile]
         private static void CreateNotifyCommand(ref EntityCommandBuffer ecb,
             in FixedString32Bytes blackboardId, in NotifyType notifyType, in Entity btInstance)
         {
